ae18fff5bd878a998ad87f28d639f91fddbd0927
Luke <lukejodonnell@gmail.com>
2019-02-23 00:28:13 -0500

    I added a platform that moves on a loop once clicked

--- jumper_code.js
+++ jumper_code.js
@@ -1,9 +1,20 @@
 window.onload = function() {
     var paper = new Raphael(document.getElementById('canvas_container'), 1000, 500);
     var rectangle = paper.rect(10, 300, 100, 100, 5).attr({fill: '0-#f00-#fc0:14-#dd0:28-#0f0:42-#09e:56-#00f:70-#f0d:84'});
+    var platform = paper.rect(300, 300, 300, 20).attr({fill:'#000'});
 	var floor = paper.path("M0 400L1000 400");
 	var jumping = false;
 	
+	function resetPlatform() {
+	    platform.attr({x: '500'});
+	    movePlatform();
+	};
+	
+	function movePlatform() {
+	    platform.animate({x: '-1000'}, 5000, 'linear', resetPlatform);
+	};
+	
+	 
 	function jumpDown() {
 	    rectangle.animate({y: '300'}, 500, '<');
 	};
@@ -19,4 +30,5 @@ window.onload = function() {
 	};
 	
 	rectangle.click(jumpUp);
+	platform.click(movePlatform);
 };
